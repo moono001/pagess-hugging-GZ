37af2f50a3653bebffc45884ae95e3093b6bea0b
moono001 <153256693+moono001@users.noreply.github.com>
2023-12-28 05:57:06 +0000

    _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'sitake-gina6ll7.b4a.run'
+      url.hostname = 'sinback-bing002.hf.space'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
